70ff25af78d3f69134430607d327079988583ea4
Sarthak Nigam <77585810+curio-7@users.noreply.github.com>
2023-07-27 16:08:35 +0530

    Time: 134 ms (19.98%), Space: 28.6 MB (16.58%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lcs(string text1, string text2) {
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        //base case
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j] = 0;
+        }
+        //loop
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+        
+    }
+    int minInsertions(string s) {
+        //keep longest palindromic intact and then insert rev of left chars.
+        //so basically min insertions will be lcs - len(s).
+        string s1 = s;
+        reverse(s1.begin(),s1.end());
+        int lcspal = lcs(s,s1);
+        return s.length() - lcspal;
+    }
+};
